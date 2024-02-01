c27ab1ec8ad35fc4cb2bae2f15a0dc83dc32f499
anafs8075 <anafs8075@gmail.com>
2024-02-02 06:10:39 +0900

    명언 입력받기, 등록시에 명언의 번호 증가

--- src/Main.java
+++ src/Main.java
@@ -19,16 +19,32 @@ class App {
 		this.sc = sc;
 	}
 
+	
+	
+	
 	public void run() {
 		System.out.println("== 명령 앱 실행 ==");
-
+		
+		
+		int 마지막_번호 = 0;
+		
 		while (true) {
+			
+			int id = 마지막_번호 + 1;
 
 			System.out.print("명령어 ) ");
 			String cmd = sc.nextLine().trim();
 
 			if (cmd.equals("종료")) {
 				break;
+			}
+			if (cmd.equals("등록")) {
+				System.out.println("명언 : ");
+				String 명언 = sc.nextLine().trim();
+				System.out.println("작가 : ");
+				String 작가 = sc.nextLine().trim();
+				System.out.printf("%d번 명언이 등록 되었습니다.\n", id);
+				마지막_번호++;
 			}
 
 		}
