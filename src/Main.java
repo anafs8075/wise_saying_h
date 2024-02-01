5e8b1eaa00b9ee825124c18ebe93251f9a53e7f3
anafs8075 <anafs8075@gmail.com>
2024-02-02 06:03:10 +0900

    1단계, 종료기능 구현

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("123");
+
+		Scanner sc = new Scanner(System.in);
+
+		new App(sc).run();
+
+		sc.close();
+
+	}
+}
+
+class App {
+	private Scanner sc;
+
+	public App(Scanner sc) {
+		this.sc = sc;
 	}
 
+	public void run() {
+		System.out.println("== 명령 앱 실행 ==");
+
+		while (true) {
+
+			System.out.print("명령어 ) ");
+			String cmd = sc.nextLine().trim();
+
+			if (cmd.equals("종료")) {
+				break;
+			}
+
+		}
+	}
 }
